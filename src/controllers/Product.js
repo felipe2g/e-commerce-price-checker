c8d3b682c858d9db52a54aa8c91f26a612ca9f52
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2019-10-24 15:48:17 -0300

    request and parse complete

--- src/controllers/Product.js
+++ src/controllers/Product.js
@@ -1,8 +1,17 @@
+const axios = require('axios');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+
+const getInfoProduct = require('../utils/getInfoProduct');
+
 module.exports = {
     async index(req, res) {
 
         const url = 'https://www.kabum.com.br/produto/104920';
     
+        const productsInfo = await getInfoProduct(104920);
+
+        console.log(productsInfo);
 
     }
 }
--- src/utils/getInfoProduct.js
+++ src/utils/getInfoProduct.js
@@ -1,9 +1,21 @@
 const axios = require('axios');
-const parse5 = require('parse5');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
 
 module.exports = async function(productId) {
-    await axios.get(productId)
-    .then(function (response) {
-        return response.data;
-    });
+    const response = await axios.request({
+        method: 'GET',
+        url: `https://www.kabum.com.br/produto/${productId}`,
+        responseType: 'arraybuffer',
+        responseEncoding: 'binary'
+      });
+
+    const dom = new JSDOM(response.data);
+    const productName = dom.window.document.querySelector(".titulo_det").textContent;
+    const discountPrice = (dom.window.document.querySelector(".preco_desconto").textContent).replace(/[^\d\,]/g, "");
+    const normalPrice = (dom.window.document.querySelector(".preco_normal").textContent).replace(/[^\d\,]/g, "");
+    // const sections = (dom.window.document.querySelector(".links_det").textContent).replace(/[^\wÀ-ú^\>]/g, "");
+    const sections = (dom.window.document.querySelector(".links_det").textContent).replace(/\s+/g, "");
+    return sections;
+
 }
