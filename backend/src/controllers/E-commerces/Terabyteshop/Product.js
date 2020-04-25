3abcd86e3d25427ed2310d89ae6111ab30d2623e
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2020-04-25 11:57:21 -0300

    started dev terabyteshop scrapper

--- backend/src/controllers/E-commerces/Terabyteshop/Product.js
+++ backend/src/controllers/E-commerces/Terabyteshop/Product.js
@@ -1,13 +1,32 @@
 const axios = require("axios");
+const cheerio = require("cheerio");
 
 module.exports = {
   async show(req, res) {
     const productId = req.params.id;
 
-    const productInfo = await axios.get(`https://www.terabyteshop.com.br/produto/${productId}/a`);
-    
-    
+    axios.get(`https://www.terabyteshop.com.br/produto/${productId}/a`)
+      .then((response) => {
+        if(response.status === 200) {
+          const html = response.data;
+          var $ = cheerio.load(html, {xmlMode: true});
+          
+          let re1 = /(<script type="application\/ld\+json">)[\s\S]*?(<\/script>)/g;
+          let re2 = /({)[\s\S]*(})/g;
 
-    res.json(productInfo.data);
+          const string = $("head").html();
+
+          const scripts = string.match(re1);
+
+          const selectedProduct = (scripts[0].match(re2));
+
+          res.send(JSON.stringify(selectedProduct));
+
+          // const productInfo = JSON.parse(string.match(re));
+
+          // res.send(productInfo);
+
+        }
+      });
   }
 };
