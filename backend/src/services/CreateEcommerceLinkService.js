b9427cfe98ba7c2f0cf3ac5d8c77326af96c6a99
felipe2g <felipeguimaraes2406@gmail.com>
2020-08-03 17:27:47 -0300

    added createlinkservice kabum

--- backend/src/services/CreateEcommerceLinkService.js
+++ backend/src/services/CreateEcommerceLinkService.js
@@ -1,7 +1,9 @@
 class CreateEcommerceLinkService {
-    async execute(){
-
+  async execute(ecommerceName, id){
+    if(ecommerceName = 'kabum'){
+      return `https://servicespub.prod.api.aws.grupokabum.com.br/descricao/v1/descricao/produto/${id}`
     }
   }
+}
   
-  export default CreateEcommerceLinkService;
+export default CreateEcommerceLinkService;
